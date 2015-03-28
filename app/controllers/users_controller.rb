adf84123a24c1d9f67c875b90f48eb328c9064d4
yuki3738 <y.minamiya@gmail.com>
2015-03-28 14:54:30 +0900

    7.4.2flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
+      redirect_to @user
     else
       render 'new'
     end
