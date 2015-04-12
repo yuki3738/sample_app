7c47158a5c703f543dc87278b38f786ce2fc5a12
yuki3738 <y.minamiya@gmail.com>
2015-04-12 19:55:23 +0900

    8.3Cucumberの紹介 (オプション)

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,7 +10,7 @@ module SessionsHelper
   def signed_in?
       !current_user.nil?
   end
-  
+
   def current_user=(user)
     @current_user = user
   end
@@ -18,5 +18,10 @@ module SessionsHelper
   def current_user
     remember_token = User.encrypt(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
+  end
+
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
   end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -22,7 +22,7 @@ describe "Authentication" do
 
       describe "after visiting another page" do
         before { click_link "Home" }
-        it { should_not have_selector('div.alert.alert-error') }        
+        it { should_not have_selector('div.alert.alert-error') }
       end
 
 
@@ -40,6 +40,11 @@ describe "Authentication" do
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
     end
   end
 end
