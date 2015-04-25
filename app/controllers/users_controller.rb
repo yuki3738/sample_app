25f9b8a2283b074741b9bb067f554370bb1e2cb8
yuki3738 <y.minamiya@gmail.com>
2015-04-25 16:14:24 +0900

    9.2.2正しいユーザーを要求する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -32,10 +33,15 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-  
+
   private
 
     def user_params
-       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
     end
 end
